d3600e9140960101264d10eb778ca34a55503867
Lautaro Pereyra <hi@imlautaro.com>
2023-08-05 18:50:38 -0300

    setup google-fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,12 +11,54 @@ export default defineNuxtConfig({
 		},
 	},
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/supabase', '@unocss/nuxt', 'nuxt-icon'],
+	googleFonts: {
+		families: {
+			'Inter': [400, 500, 700],
+			'Roboto+Slab': [400, 500, 700],
+		},
+	},
+	modules: [
+		'@nuxtjs/supabase',
+		'@unocss/nuxt',
+		'nuxt-icon',
+		'@nuxtjs/google-fonts',
+	],
 	supabase: {
 		redirect: false,
 	},
 	unocss: {
 		preflight: true,
+		preflights: [
+			{
+				getCSS: ({ theme: _theme }) => {
+					const theme = _theme as {
+						fontFamily?: {
+							sans?: string | string[]
+						}
+					}
+
+					let bodyFont: null | string = null
+
+					if (theme.fontFamily?.sans) {
+						if (typeof theme.fontFamily.sans === 'string') {
+							bodyFont = theme.fontFamily.sans
+						} else {
+							bodyFont = theme.fontFamily.sans.join(', ')
+						}
+					}
+
+					return `
+						html {
+							-webkit-tap-highlight-color: transparent;
+							scroll-behavior: smooth;
+							user-select: none;
+						}
+						*, *:focus { outline: none !important; }
+						${bodyFont && `body { font-family: ${bodyFont}; }`}
+					`
+				},
+			},
+		],
 		safelist: [
 			...range(12).map(i => `space-x-${i}`),
 			...range(12).map(i => `space-y-${i}`),
@@ -35,6 +77,12 @@ export default defineNuxtConfig({
 		],
 		shortcuts: {
 			'container-px': 'px-6',
+		},
+		theme: {
+			fontFamily: {
+				serif: ['"Roboto Slab"', 'sans-serif'],
+				sans: ['Inter', 'sans-serif'],
+			},
 		},
 		transformers: [transformerDirectives(), transformerVariantGroup()],
 		typography: true,
