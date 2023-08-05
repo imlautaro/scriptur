ac878a928126638468dc1c204dfdd4cd2c7cd920
Lautaro Pereyra <hi@imlautaro.com>
2023-08-05 18:30:44 -0300

    setup nuxt-icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,13 @@ import transformerDirectives from '@unocss/transformer-directives'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineNuxtConfig({
+	appConfig: {
+		nuxtIcon: {
+			size: '1.5em',
+		},
+	},
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/supabase', '@unocss/nuxt'],
+	modules: ['@nuxtjs/supabase', '@unocss/nuxt', 'nuxt-icon'],
 	supabase: {
 		redirect: false,
 	},
