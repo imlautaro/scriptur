d7b29161c6ece558babffb8ec392efa69f4b3444
Lautaro Pereyra <hi@imlautaro.com>
2023-08-05 18:29:29 -0300

    setup unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/supabase'],
+	modules: ['@nuxtjs/supabase', '@unocss/nuxt'],
+	unocss: {
+		preflight: true,
+		transformers: [transformerDirectives(), transformerVariantGroup()],
+		typography: true,
+	},
 })
