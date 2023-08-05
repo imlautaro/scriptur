1fc0b9dcef25eb6e1aee3a01c330c0afc0da612a
Lautaro Pereyra <hi@imlautaro.com>
2023-08-05 18:44:12 -0300

    create stack component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import transformerDirectives from '@unocss/transformer-directives'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
 
+const range = (size: number, startAt = 1) =>
+	Array.from(Array(size).keys()).map(i => i + startAt)
+
 export default defineNuxtConfig({
 	appConfig: {
 		nuxtIcon: {
@@ -14,6 +17,22 @@ export default defineNuxtConfig({
 	},
 	unocss: {
 		preflight: true,
+		safelist: [
+			...range(12).map(i => `space-x-${i}`),
+			...range(12).map(i => `space-y-${i}`),
+			...range(12).map(i => `px-${i}`),
+			'items-start',
+			'items-baseline',
+			'items-center',
+			'items-stretch',
+			'items-end',
+			'justify-start',
+			'justify-end',
+			'justify-center',
+			'justify-between',
+			'justify-around',
+			'justify-evenly',
+		],
 		transformers: [transformerDirectives(), transformerVariantGroup()],
 		typography: true,
 	},
