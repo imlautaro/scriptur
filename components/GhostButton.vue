6cefafc461946b1e3ebdcf4519bb4656505be7b8
Lautaro Pereyra <hi@imlautaro.com>
2023-08-08 12:11:55 -0300

    add ghost-button component

---
+++ components/GhostButton.vue
@@ -0,0 +1,13 @@
+<template>
+	<Stack
+		class="duration-150 hover:bg-black/5 active:bg-black/10"
+		v-bind="{
+			...(() =>
+				!$attrs.component && !$attrs.to
+					? { component: 'button' }
+					: {})(),
+		}"
+	>
+		<slot />
+	</Stack>
+</template>
