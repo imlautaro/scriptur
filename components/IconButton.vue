461bbf8764479b3e247c750848960c30c20a47d6
Lautaro Pereyra <hi@imlautaro.com>
2023-08-08 12:09:27 -0300

    add icon-button component

---
+++ components/IconButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+	icon?: string
+	white?: boolean
+}>()
+</script>
+
+<template>
+	<Stack
+		class="duration-150 border p-2 rounded-full"
+		:class="[
+			white
+				? 'hover:bg-white/5 active:bg-white/10'
+				: 'hover:bg-black/5 active:bg-black/10',
+		]"
+		v-bind="{
+			...(() =>
+				!$attrs.component && !$attrs.to
+					? { component: 'button' }
+					: {})(),
+		}"
+	>
+		<Icon v-if="!$slots.default && icon" :name="icon" />
+		<slot />
+	</Stack>
+</template>
