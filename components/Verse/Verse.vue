75f49e45226b25b42299d417a8bad7f075a9a22b
Lautaro Pereyra <hi@imlautaro.com>
2023-08-08 12:06:27 -0300

    add verse component

---
+++ components/Verse/Verse.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+	book: number
+	chapter: number
+	verse: number
+	content: string
+	heading?: string
+}>()
+
+const customizationsStore = useCustomizationsStore()
+</script>
+
+<template>
+	<VerseHeading v-if="heading">
+		{{ heading }}
+	</VerseHeading>
+	<div class="relative">
+		<span
+			class="absolute -left-1 -translate-x-full text-[0.5em] top-0 -translate-y-1 text-gray select-none"
+			:class="[customizationsStore.serif && 'font-serif']"
+		>
+			{{ verse }}
+		</span>
+		<div
+			class="leading-relaxed"
+			:class="[customizationsStore.serif && 'font-serif']"
+			v-html="removeSupElement(content)"
+		/>
+	</div>
+</template>
---
+++ components/Verse/VerseHeading.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const customizationsStore = useCustomizationsStore()
+</script>
+
+<template>
+	<h2
+		class="font-bold text-xl"
+		:class="[customizationsStore.serif && 'font-serif']"
+	>
+		<slot />
+	</h2>
+</template>
