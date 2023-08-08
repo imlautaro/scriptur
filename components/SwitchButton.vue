482a745dcc545f7611a2b5ee12de44292e2834d4
Lautaro Pereyra <hi@imlautaro.com>
2023-08-08 12:09:36 -0300

    add switch-button component

---
+++ components/SwitchButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{ modelValue: boolean }>()
+defineEmits(['update:modelValue'])
+
+const customizationsStore = useCustomizationsStore()
+</script>
+
+<template>
+	<button
+		@click="$emit('update:modelValue', !modelValue)"
+		class="duration-300 rounded-full h-8 w-14 relative"
+		:class="[
+			modelValue
+				? customizationsStore.sepia
+					? 'bg-black'
+					: 'bg-primary'
+				: 'bg-gray-200',
+		]"
+	>
+		<div
+			class="duration-150 w-6 h-6 rounded-full bg-white shadow absolute top-1 left-1"
+			:class="[modelValue ? 'translate-x-6' : 'translate-x-0']"
+		/>
+	</button>
+</template>
