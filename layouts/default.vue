b38d5f24340d96d566be31fc26efa9838793b3d3
Lautaro Pereyra <hi@imlautaro.com>
2023-08-13 10:59:17 -0300

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const customizations = useCustomizationsStore()
+</script>
+
+<template>
+	<Stack
+		class="fixed h-full w-full top-0 left-0 overflow-hidden"
+		:class="[customizations.sepia && 'brightness-75 sepia contrast-75']"
+		vertical
+	>
+		<Stack class="h-full flex-1 overflow-hidden relative">
+			<Stack
+				class="bg-white flex-1 h-full relative overflow-hidden"
+				vertical
+			>
+				<slot />
+			</Stack>
+		</Stack>
+		<BottomNavigation />
+	</Stack>
+</template>
