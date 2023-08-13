1d2bdf5741c3168e57a98ad5c2f3dd733b5f4a0d
Lautaro Pereyra <hi@imlautaro.com>
2023-08-13 10:58:22 -0300

    add list-card component

---
+++ components/ListCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+	title?: string
+}>()
+</script>
+
+<template>
+	<Stack class="bg-white rounded-2xl border overflow-hidden" vertical>
+		<span v-if="title" class="pt-3 px-5 text-sm text-gray">
+			{{ title }}
+		</span>
+		<slot />
+	</Stack>
+</template>
