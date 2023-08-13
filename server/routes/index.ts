1ea8c48ef8681019d41b99a1212cac8e13777236
Lautaro Pereyra <hi@imlautaro.com>
2023-08-13 19:28:00 -0300

    fix redirect with an undefined i18n cookie

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,8 @@
 export default defineEventHandler(event => {
 	const { book, chapter } = getLastVisited(event)
 	const i18nRedirected = getCookie(event, 'i18n_redirected')
-	const prefix = i18nRedirected === 'en' ? '' : `/${i18nRedirected}`
+	const prefix =
+		!i18nRedirected || i18nRedirected === 'en' ? '' : `/${i18nRedirected}`
 
 	return sendRedirect(event, `${prefix}/read/${book}/${chapter}`)
 })
