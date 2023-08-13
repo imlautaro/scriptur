d7d620169fd7ee669e88055aa5fbe56a43700fda
Lautaro Pereyra <hi@imlautaro.com>
2023-08-13 10:59:12 -0300

    add bottom-navigation component

---
+++ components/BottomNavigation/BottomNavigation.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const lastVisited = useLastVisited()
+const show = computed(() => true)
+</script>
+
+<template>
+	<Transition name="bottom-navigation">
+		<Stack
+			v-if="show"
+			class="bg-white border-t h-full max-h-18 duration-300"
+			vertical
+		>
+			<Stack class="h-18">
+				<BottomNavigationItem
+					:label="$t('read')"
+					icon="solar:book-bold-duotone"
+					:to="`/read/${lastVisited.book}/${lastVisited.chapter}`"
+					:active="
+						$route.name
+							?.toString()
+							.startsWith('read-book-chapter___')
+					"
+				/>
+				<BottomNavigationItem
+					:label="$t('books-title')"
+					icon="solar:library-bold-duotone"
+					to="/read/books"
+					:active="
+						$route.name?.toString().startsWith('read-books___')
+					"
+				/>
+				<BottomNavigationItem
+					:label="`${$t('my')} ${$t('space')}`"
+					icon="solar:widget-2-bold"
+					to="/myspace"
+					:active="$route.name?.toString().startsWith('myspace___')"
+				/>
+			</Stack>
+		</Stack>
+	</Transition>
+</template>
+
+<style>
+.bottom-navigation-enter-from,
+.bottom-navigation-leave-to {
+	@apply max-h-0;
+}
+</style>
---
+++ components/BottomNavigation/BottomNavigationItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+	label: string
+	icon: string
+	active?: boolean
+}>()
+
+const customizations = useCustomizationsStore()
+</script>
+
+<template>
+	<Stack
+		class="duration-300 flex-1 group"
+		:class="[
+			active
+				? customizations.sepia
+					? 'text-black'
+					: 'text-primary-600'
+				: customizations.sepia
+				? 'text-gray-400'
+				: 'text-gray-600',
+		]"
+		gap="1"
+		items="center"
+		justify="center"
+		vertical
+		@contextmenu.prevent
+	>
+		<Stack
+			class="duration-300 py-1 rounded-full"
+			:class="[
+				active
+					? customizations.sepia
+						? 'bg-black/10 px-4'
+						: 'bg-primary-600/10 px-4'
+					: 'px-1 group-active:(bg-black/5 px-4)',
+			]"
+		>
+			<Icon class="h-6 w-6" :name="icon" />
+		</Stack>
+		<span class="font-medium text-xs" v-text="label" />
+	</Stack>
+</template>
